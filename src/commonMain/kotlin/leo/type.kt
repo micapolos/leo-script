9f311fb824f85c778d4a0ae9402eaa9b7e0239d5
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-29 22:48:48 +0200

    type

---
+++ src/commonMain/kotlin/leo/type.kt
@@ -0,0 +1,16 @@
+package leo
+
+sealed class Type
+
+object EmptyType: Type()
+data class FieldType(val field: TypeField): Type()
+
+data class TypeField(val name: String, val rhs: TypeRhs)
+
+sealed class TypeRhs
+data class ClassTypeRhs(val class_ : Class<*>): TypeRhs()
+data class ArrowTypeRhs(val arrow: Arrow): TypeRhs()
+data class TypeTypeRhs(val type: Type): TypeRhs()
+data class ValueTypeRhs(val value: Value): TypeRhs()
+
+data class Arrow(val lhsType: Type, val rhsType: Type)
