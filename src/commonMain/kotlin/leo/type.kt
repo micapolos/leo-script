8bf5a6fb90a35df6ed238b238c36670ffec1a6e5
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-29 23:00:54 +0200

    type

--- src/commonMain/kotlin/leo/type.kt
+++ src/commonMain/kotlin/leo/type.kt
@@ -1,5 +1,7 @@
 package leo
 
+import kotlin.reflect.KClass
+
 sealed class Type
 
 object EmptyType: Type()
@@ -8,7 +10,7 @@ data class FieldType(val field: TypeField): Type()
 data class TypeField(val name: String, val rhs: TypeRhs)
 
 sealed class TypeRhs
-data class ClassTypeRhs(val class_ : Class<*>): TypeRhs()
+data class ClassTypeRhs(val class_ : KClass<*>): TypeRhs()
 data class ArrowTypeRhs(val arrow: Arrow): TypeRhs()
 data class TypeTypeRhs(val type: Type): TypeRhs()
 data class ValueTypeRhs(val value: Value): TypeRhs()
