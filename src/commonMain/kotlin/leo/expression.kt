6293b2b9f952336265ee221c762a9ab7bd81882c
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-05-30 22:45:09 +0200

    script.syntax.fixes

--- src/commonMain/kotlin/leo/expression.kt
+++ src/commonMain/kotlin/leo/expression.kt
@@ -2,4 +2,4 @@ package leo
 
 // TODO: Refactor to proper expression model.
 data class Expression(val script: Script)
-fun expression(script: Script) = Expression(script.syntax.script) // TODO: For validation
+fun expression(script: Script) = Expression(script)
