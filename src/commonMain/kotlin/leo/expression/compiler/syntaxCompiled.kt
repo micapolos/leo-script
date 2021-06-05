1bd7c42b2d166a39f1da04ddebc6b8a77380c77b
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-06 01:26:50 +0200

    delete expression

--- src/commonMain/kotlin/leo/expression/compiler/syntaxCompiled.kt
+++
@@ -1,9 +0,0 @@
-package leo.expression.compiler
-
-import leo.Syntax
-
-val Syntax.compiledExpression: CompiledExpression get() =
-	compiler().compiledExpression(this)
-
-val Syntax.compiledVector: CompiledVector get() =
-	compiler().compiledVector(this)
