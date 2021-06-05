d54e6a367879d531233f249d228f44f0bb1705e3
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-05 15:36:41 +0200

    typed tests

--- src/commonTest/kotlin/leo/syntaxTypedTest.kt
+++ src/commonTest/kotlin/leo/syntaxTypedTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.Test
 
 class SyntaxTypedTest {
 	@Test
-	fun literal() {
+	fun literals() {
 		syntax(
 			line(syntaxAtom(literal(10))),
 			line(syntaxAtom(literal("Hello, world!"))))
@@ -17,5 +17,29 @@ class SyntaxTypedTest {
 					type(
 						numberTypeLine,
 						textTypeLine)))
+	}
+
+	@Test
+	fun fields() {
+		syntax(
+			"x" lineTo syntax("zero"),
+			"y" lineTo syntax("one"))
+			.typed
+			.assertEqualTo(
+				expression(
+					line(
+						atom(
+							"x" fieldTo expression(
+								line(atom("zero" fieldTo emptyTyped)))
+								.of(type("zero" lineTo type())))),
+					line(
+						atom(
+							"y" fieldTo expression(
+								line(atom("one" fieldTo emptyTyped)))
+								.of(type("one" lineTo type())))))
+					.of(
+						type(
+							"x" lineTo type("zero" lineTo type()),
+							"y" lineTo type("one" lineTo type()))))
 	}
 }
