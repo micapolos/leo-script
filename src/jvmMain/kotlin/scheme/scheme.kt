4dcad1d03e039592ae84277ddfdd2ab4c48eb142
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-05 10:39:20 +0200

    int.scheme double.scheme

--- src/jvmMain/kotlin/scheme/scheme.kt
+++ src/jvmMain/kotlin/scheme/scheme.kt
@@ -1,6 +1,5 @@
 package scheme
 
-import leo.Number
 import leo.Text
 import leo.literalString
 
@@ -8,5 +7,6 @@ data class Scheme(val string: String)
 val String.scheme get() = Scheme(this)
 
 val Text.scheme: Scheme get() = string.literalString.scheme
-val Number.scheme: Scheme get() = toString().scheme
+val Int.scheme: Scheme get() = toString().scheme
+val Double.scheme: Scheme get() = toString().scheme
 val Boolean.scheme: Scheme get() = if (this) "#t".scheme else "#f".scheme
--- src/jvmTest/kotlin/scheme/termSchemeTest.kt
+++ src/jvmTest/kotlin/scheme/termSchemeTest.kt
@@ -11,6 +11,9 @@ class TermSchemeTest {
 		lambda(lambda(v<Scheme>(1)(v(0)))).scheme.string.assertEqualTo("(lambda v0 (lambda v1 (v0 v1)))")
 		term("string-append".scheme).scheme.string.assertEqualTo("string-append")
 		term("Hello, world!".text.scheme).scheme.string.assertEqualTo("\"Hello, world!\"")
-		term(10.number.scheme).scheme.string.assertEqualTo("10")
+		term(10.scheme).scheme.string.assertEqualTo("10")
+		term(3.14.scheme).scheme.string.assertEqualTo("3.14")
+		term(false.scheme).scheme.string.assertEqualTo("#f")
+		term(true.scheme).scheme.string.assertEqualTo("#t")
 	}
 }
