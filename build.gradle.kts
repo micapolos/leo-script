6255da97fe0e2bc33b7e53488f08d94f07a6bf95
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-12 09:47:33 +0200

    no macos

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,6 @@ repositories {
 }
 
 kotlin {
-//    macosX64 {
-//        binaries {
-//            executable {
-//                entryPoint = "leo.main"
-//            }
-//        }
-//    }
     jvm {
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
@@ -53,8 +46,6 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-//        val macosX64Main by getting
-//        val macosX64Test by getting
         val jvmMain by getting
         val jvmTest by getting
     }
