8a10bb863c9cc383c220179c7edb2d96d4bdad9b
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-13 11:52:19 +0200

    environment

--- src/commonMain/kotlin/leo/named/compiler/binding.kt
+++ src/commonMain/kotlin/leo/named/compiler/binding.kt
@@ -11,9 +11,14 @@ import leo.named.typed.typed
 
 data class Binding(val type: Type, val isConstant: Boolean)
 
-fun binding(type: Type, isConstant: Boolean): Binding = Binding(type, isConstant)
-fun constantBinding(type: Type) = binding(type, isConstant = true)
-fun functionBinding(type: Type) = binding(type, isConstant = false)
+fun binding(type: Type, isConstant: Boolean): Binding =
+	Binding(type, isConstant)
+
+fun constantBinding(type: Type) =
+	binding(type, isConstant = true)
+
+fun functionBinding(type: Type) =
+	binding(type, isConstant = false)
 
 fun <T> Binding.resolve(typedExpression: TypedExpression<T>): TypedExpression<T> =
 	typed(expression(typedExpression), type)
