19abfb0a91cea48fbba96b638d7d85a9ec062350
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-14 07:59:08 +0200

    prelude functions

--- src/commonMain/kotlin/leo/named/library/context.kt
+++ src/commonMain/kotlin/leo/named/library/context.kt
@@ -5,12 +5,19 @@ import leo.named.compiler.Context
 import leo.named.compiler.context
 import leo.named.compiler.plus
 import leo.natives.minusName
+import leo.natives.timesName
 import leo.numberTypeLine
 import leo.plusName
+import leo.textName
+import leo.textTypeLine
 import leo.type
 
 val preludeContext: Context get() =
 	context()
+		.plus(
+			type(textName lineTo type(numberTypeLine)),
+			type(textTypeLine),
+			numberTextFunction)
 		.plus(
 			type(numberTypeLine, plusName lineTo type(numberTypeLine)),
 			type(numberTypeLine),
@@ -19,3 +26,7 @@ val preludeContext: Context get() =
 			type(numberTypeLine, minusName lineTo type(numberTypeLine)),
 			type(numberTypeLine),
 			numberMinusNumberFunction)
+		.plus(
+			type(numberTypeLine, timesName lineTo type(numberTypeLine)),
+			type(numberTypeLine),
+			numberTimesNumberFunction)
--- src/commonMain/kotlin/leo/named/library/functions.kt
+++ src/commonMain/kotlin/leo/named/library/functions.kt
@@ -5,8 +5,17 @@ import leo.named.expression.function
 import leo.named.value.double
 import leo.named.value.get
 import leo.named.value.numberValue
+import leo.named.value.textValue
+import leo.natives.minusName
+import leo.natives.timesName
 import leo.numberName
 import leo.plusName
+import leo.textName
+
+val numberTextFunction get() =
+	function("numberText") {
+		get(textName).get(numberName).double.toString().textValue
+	}
 
 val numberPlusNumberFunction get() =
 	function("numberPlusNumber") {
@@ -18,6 +27,13 @@ val numberPlusNumberFunction get() =
 val numberMinusNumberFunction get() =
 	function("numberMinusNumber") {
 		get(numberName).double
-			.minus(get(plusName).get(numberName).double)
+			.minus(get(minusName).get(numberName).double)
+			.numberValue
+	}
+
+val numberTimesNumberFunction get() =
+	function("numberTimesNumber") {
+		get(numberName).double
+			.times(get(timesName).get(numberName).double)
 			.numberValue
 	}
