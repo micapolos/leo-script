477aba02a55cb1dca0c7f3e68cd8eb7d28c43d35
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-26 15:33:31 +0200

    monads

---
+++ src/commonMain/kotlin/leo/monad.kt
@@ -0,0 +1,45 @@
+package leo
+
+data class Monad(
+	val returnFn: (Value) -> Evaluation<Value>,
+	val bindFn: (Value, (Value) -> Evaluation<Value>) -> Evaluation<Value>)
+
+data class MonadDefinition(val pattern: Value, val monad: Monad)
+
+data class MonadDictionary(val definitionStack: Stack<MonadDefinition>)
+
+fun definition(pattern: Value, monad: Monad) =
+	MonadDefinition(pattern, monad)
+
+fun dictionary(definition: MonadDefinition, vararg definitions: MonadDefinition) =
+	MonadDictionary(stack(stackLink(definition, *definitions)))
+
+fun monadDictionary(vararg definitions: MonadDefinition) =
+	MonadDictionary(stack(*definitions))
+
+fun MonadDictionary.plus(definition: MonadDefinition): MonadDictionary =
+	definitionStack.push(definition).let(::MonadDictionary)
+
+val identityMonad get() =
+	Monad(
+		{ value -> value.evaluation },
+		{ value, fn -> fn(value) })
+
+val optionMonad get() =
+	Monad(
+		{ value -> value.liftToOption.evaluation },
+		{ value, fn -> value.optionBindEvaluation(fn) })
+
+// TODO: Implement "one of" pattern.
+val presentOptionMonadDefinition =
+	definition(
+		value(optionName fieldTo value(presentName fieldTo anyValue)),
+		optionMonad)
+
+val absentOptionMonadDefinition = definition(
+	value(optionName fieldTo value(absentName)),
+	optionMonad)
+
+val preludeMonadDictionary = dictionary(
+	presentOptionMonadDefinition,
+	absentOptionMonadDefinition)
