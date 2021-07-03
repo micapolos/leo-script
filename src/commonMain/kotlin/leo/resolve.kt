d63214274d5643bb8b14199dbed3e1324940b28c
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-03 16:29:35 +0200

    given.definition

--- src/commonMain/kotlin/leo/resolve.kt
+++ src/commonMain/kotlin/leo/resolve.kt
@@ -1,8 +1,5 @@
 package leo
 
-import leo.base.fold
-import leo.base.reverse
-
 fun Dictionary.resolveEvaluation(value: Value): Evaluation<Value> =
 	applyOrNullEvaluation(value)
 		.or { value.resolveEvaluation }
@@ -14,25 +11,5 @@ fun Dictionary.applyOrNullEvaluation(value: Value): Evaluation<Value?> =
 fun Dictionary.applyEvaluationOrNull(value: Value): Evaluation<Value>? =
 	applicationOrNull(value)?.applyEvaluation(value)
 
-fun Dictionary.plus(value: Value): Dictionary =
-	this
-		.plusGiven(value)
-		.plusFields(value)
-		.plus(given(value))
-
 fun Dictionary.plus(given: ValueGiven) =
 	plus(definition(given))
-
-fun Dictionary.plusGiven(value: Value) =
-	plus(definition(value(givenName), binding(value(givenName fieldTo value))))
-
-fun Dictionary.plusFields(value: Value) =
-	fold(value.fieldSeq.reverse) { plus(it) }
-
-fun Dictionary.plus(field: Field): Dictionary =
-	plus(
-		definition(
-			value(field.name),
-			binding(value(field))
-		)
-	)
