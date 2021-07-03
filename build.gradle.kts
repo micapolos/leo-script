04e4dd1bec0cc6df0c7b0d440701468415608755
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-03 16:33:41 +0200

    given.definition

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "micapolos"
-version = "0.25.10"
+version = "0.25.11"
 
 repositories {
     mavenCentral()
--- src/commonMain/kotlin/leo/version.kt
+++ src/commonMain/kotlin/leo/version.kt
@@ -1,3 +1,3 @@
 package leo
 
-val version = "0.25.10"
+val version = "0.25.11"
