bcd2b7a4a0663e1ddc8f139f35c70f3502464031
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-03 17:55:41 +0200

    version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "micapolos"
-version = "0.25.11"
+version = "0.25.12"
 
 repositories {
     mavenCentral()
--- src/commonMain/kotlin/leo/version.kt
+++ src/commonMain/kotlin/leo/version.kt
@@ -1,3 +1,3 @@
 package leo
 
-val version = "0.25.11"
+val version = "0.25.12"
