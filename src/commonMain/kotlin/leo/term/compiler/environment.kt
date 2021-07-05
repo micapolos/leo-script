d9c78329ac3363eff335795dc84ed32f693114d5
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-05 20:33:02 +0200

    do

--- src/commonMain/kotlin/leo/term/compiler/environment.kt
+++ src/commonMain/kotlin/leo/term/compiler/environment.kt
@@ -2,19 +2,18 @@ package leo.term.compiler
 
 import leo.Literal
 import leo.lineTo
-import leo.numberName
 import leo.numberTypeLine
 import leo.term.anyDouble
 import leo.term.anyFn
 import leo.term.anyValue
 import leo.term.fn
+import leo.term.head
 import leo.term.invoke
 import leo.term.native
+import leo.term.tail
 import leo.term.term
 import leo.term.typed.TypedLine
 import leo.term.typed.TypedTerm
-import leo.term.typed.getOrNull
-import leo.term.typed.make
 import leo.term.typed.typed
 import leo.term.typed.typedLine
 import leo.term.value
@@ -30,18 +29,20 @@ val runtimeEnvironment: Environment<Any?> get() =
 		{ literal -> typedLine(literal) },
 		{ typedTerm ->
 			when (typedTerm.t) {
-				// TODO: Add level of indirection.
 				type(numberTypeLine, "add" lineTo type(numberTypeLine)) ->
 					typed(
 						fn(
 							fn(
-						term(anyFn {
-							value(1.variable).native.anyDouble
-								.plus(value(0.variable).native.anyDouble)
-								.anyValue
-						}))
-						).invoke(typedTerm.make("given").getOrNull(numberName)!!.v)
-							.invoke(typedTerm.make("given").getOrNull("add")!!.getOrNull(numberName)!!.v),
+								term(
+									anyFn {
+										value(1.variable).native.anyDouble
+											.plus(value(0.variable).native.anyDouble)
+											.anyValue
+									}
+								)
+							)
+							// TODO: Add level of indirection for binary operators.
+						).invoke(typedTerm.v.tail).invoke(typedTerm.v.head),
 						type(numberTypeLine))
 				else -> null
 			}
