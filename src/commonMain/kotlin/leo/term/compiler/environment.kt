dec48c905dd525ba16a92ab96ef9c7c175dc9f2b
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-09 16:07:56 +0200

    type error

--- src/commonMain/kotlin/leo/term/compiler/environment.kt
+++ src/commonMain/kotlin/leo/term/compiler/environment.kt
@@ -2,10 +2,14 @@ package leo.term.compiler
 
 import leo.Literal
 import leo.Script
+import leo.isEmpty
+import leo.lineTo
+import leo.plus
+import leo.script
 import leo.term.Term
 import leo.term.typed.TypedLine
 import leo.term.typed.TypedTerm
-import leo.term.typed.prefix
+import leo.term.typed.infix
 import leo.term.typed.typed
 import leo.typeLine
 import leo.typeName
@@ -22,8 +26,17 @@ fun <V> Environment<V>.typedLine(literal: Literal): TypedLine<V> =
   typed(literalFn(literal), literal.typeLine)
 
 fun <V> Environment<V>.resolveTypeOrNull(typedTerm: TypedTerm<V>): TypedTerm<V>? =
-  typedTerm.prefix(typeName) { rhs ->
-    resolveType(rhs)
+  typedTerm.infix(typeName) { lhs, rhs ->
+    if (!lhs.t.isEmpty) compileError(
+      lhs.t.script
+        .plus("type" lineTo rhs.t.script)
+        .plus("is" lineTo script(
+          "not" lineTo script(
+            "matching" lineTo script(
+              "type" lineTo script(
+                "any" lineTo script(
+                  "expression")))))))
+    else resolveType(rhs)
   }
 
 fun <V> Script.typedTerm(environment: Environment<V>): TypedTerm<V> =
