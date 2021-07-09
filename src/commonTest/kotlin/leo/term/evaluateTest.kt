374792950342b4c0c5b0c55dcd3281e3b9dd692b
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-10 00:29:18 +0200

    empty.term empty.value

--- src/commonTest/kotlin/leo/term/evaluateTest.kt
+++ src/commonTest/kotlin/leo/term/evaluateTest.kt
@@ -49,4 +49,49 @@ class EvaluateTest {
       .value(evaluator())
       .assertEqualTo("two".nativeValue)
   }
+
+  @Test
+  fun switch_oneOfThree() {
+    "foo".nativeTerm.eitherFirst.eitherFirst
+      .invoke(fn(get<String>(0).invoke(id()).invoke(id())))
+      .invoke(id())
+      .value(evaluator())
+      .assertEqualTo("foo".nativeValue)
+
+    "foo".nativeTerm.eitherFirst.eitherFirst
+      .invoke(fn(get<String>(0).invoke(fn("one".nativeTerm)).invoke(fn("two".nativeTerm))))
+      .invoke(fn("three".nativeTerm))
+      .value(evaluator())
+      .assertEqualTo("one".nativeValue)
+  }
+
+  @Test
+  fun switch_twoOfThree() {
+    "foo".nativeTerm.eitherSecond.eitherFirst
+      .invoke(fn(get<String>(0).invoke(id()).invoke(id())))
+      .invoke(id())
+      .value(evaluator())
+      .assertEqualTo("foo".nativeValue)
+
+    "foo".nativeTerm.eitherSecond.eitherFirst
+      .invoke(fn(get<String>(0).invoke(fn("one".nativeTerm)).invoke(fn("two".nativeTerm))))
+      .invoke(fn("three".nativeTerm))
+      .value(evaluator())
+      .assertEqualTo("two".nativeValue)
+  }
+
+  @Test
+  fun switch_threeOfThree() {
+    "foo".nativeTerm.eitherSecond
+      .invoke(fn(get<String>(0).invoke(id()).invoke(id())))
+      .invoke(id())
+      .value(evaluator())
+      .assertEqualTo("foo".nativeValue)
+
+    "foo".nativeTerm.eitherSecond
+      .invoke(fn(get<String>(0).invoke(fn("one".nativeTerm)).invoke(fn("two".nativeTerm))))
+      .invoke(fn("three".nativeTerm))
+      .value(evaluator())
+      .assertEqualTo("three".nativeValue)
+  }
 }
