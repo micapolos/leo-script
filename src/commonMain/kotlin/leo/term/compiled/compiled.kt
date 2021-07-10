f0703b2934b85f21d8d21a23fe3facfc7a5022fe
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-10 23:43:15 +0200

    compiled

--- src/commonMain/kotlin/leo/term/compiled/compiled.kt
+++ src/commonMain/kotlin/leo/term/compiled/compiled.kt
@@ -1,39 +1,42 @@
 package leo.term.compiled
 
+import leo.Empty
 import leo.Stack
 import leo.Type
 import leo.TypeLine
+import leo.term.IndexVariable
 
 data class Compiled<out V>(val expression: Expression<V>, val type: Type)
 
-data class Expression<out V>(val operationStack: Stack<Operation<V>>)
-
-sealed class Operation<out V>
-data class LineOperation<V>(val line: Line<V>): Operation<V>()
-data class ContentOperation<V>(val content: Content): Operation<V>()
-data class GetOperation<V>(val get: Get): Operation<V>()
-data class FunctionOperation<V>(val function: Function<V>): Operation<V>()
-data class ApplyOperation<V>(val apply: Apply<V>): Operation<V>()
-data class SelectOperation<V>(val select: Select<V>): Operation<V>()
-data class SwitchOperation<V>(val switch: Switch<V>): Operation<V>()
+sealed class Expression<out V>
+data class EmptyExpression<out V>(val empty: Empty): Expression<V>()
+data class AppendExpression<V>(val append: Append<V>): Expression<V>()
+data class ContentExpression<V>(val content: Content): Expression<V>()
+data class GetExpression<V>(val get: Get<V>): Expression<V>()
+data class FunctionExpression<V>(val function: Function<V>): Expression<V>()
+data class ApplyExpression<V>(val apply: Apply<V>): Expression<V>()
+data class VariableExpression<out V>(val variable: IndexVariable): Expression<V>()
+data class SelectExpression<V>(val select: Select<V>): Expression<V>()
+data class SwitchExpression<V>(val switch: Switch<V>): Expression<V>()
 
 sealed class Line<out V>
 data class NativeLine<V>(val native: V): Line<V>()
 data class FieldLine<V>(val field: Field<V>): Line<V>()
+data class Field<out V>(val name: String, val rhs: Compiled<V>)
+
+data class Select<out V>(val lineStack: Stack<SelectLine<V>>)
 
 sealed class SelectLine<out V>
 data class PickSelectLine<V>(val pick: Pick<V>): SelectLine<V>()
-data class DropSelectLine<V>(val drop: Drop<V>): SelectLine<V>()
+data class DropSelectLine<V>(val drop: Drop): SelectLine<V>()
+data class Pick<out V>(val line: Line<V>)
+data class Drop(val typeLine: TypeLine)
 
-data class SwitchLine<out V>(val name: String, val body: Compiled<V>)
+data class Function<out V>(val type: Type, val body: Body<V>)
 data class Body<out V>(val compiled: Compiled<V>, val isRecursive: Boolean)
 
 object Content
-data class Field<out V>(val name: String, val rhs: Compiled<V>)
-data class Get(val name: String)
-data class Apply<out V>(val rhs: Compiled<V>)
-data class Function<out V>(val type: Type, val body: Body<V>)
-data class Pick<out V>(val line: Line<V>)
-data class Drop<out V>(val typeLine: TypeLine)
-data class Select<out V>(val lineStack: Stack<SelectLine<V>>)
-data class Switch<out V>(val lineStack: Stack<SwitchLine<V>>)
+data class Append<out V>(val lhs: Compiled<V>, val line: Line<V>)
+data class Get<V>(val lhs: Compiled<V>, val index: Int)
+data class Apply<out V>(val lhs: Compiled<V>, val rhs: Compiled<V>)
+data class Switch<out V>(val lhs: Compiled<V>, val functionStack: Stack<Function<V>>)
