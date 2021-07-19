7a4bb44bc19019ca7ce91f1965b4f1f991d40b9d
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-19 03:53:54 +0200

    remove typed

--- src/commonMain/kotlin/leo/termCompilation.kt
+++
@@ -1,6 +0,0 @@
-package leo
-
-typealias TermCompilation<T> = Stateful<TermCompiler<T>, Term<T>>
-
-fun <T> Typed.termCompilation(): TermCompilation<T> = TODO()
-
--- src/commonMain/kotlin/leo/termCompiler.kt
+++
@@ -1,9 +0,0 @@
-package leo
-
-data class TermCompiler<T>(
-  val typedTermOrNullFn: (Typed) -> Term<T>?,
-  val literalTermFn: (Literal) -> Term<T>
-)
-
-fun <T> TermCompiler<T>.term(typed: Typed): Term<T> =
-  typed.termCompilation<T>().run(this).value
