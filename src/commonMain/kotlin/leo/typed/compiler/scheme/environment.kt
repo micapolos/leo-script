7ace8cd8eff169bb791e85954af2519a2ed8cc21
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-20 22:11:24 +0200

    types environment

--- src/commonMain/kotlin/leo/typed/compiler/scheme/environment.kt
+++ src/commonMain/kotlin/leo/typed/compiler/scheme/environment.kt
@@ -21,7 +21,6 @@ import leo.typed.compiled.nativeLine
 import leo.typed.compiled.onlyCompiledLine
 import leo.typed.compiled.prefix
 import leo.typed.compiler.Environment
-import leo.typed.compiler.native.nativeTypesEnvironment
 import scheme.Scheme
 import scheme.scheme
 
@@ -31,7 +30,7 @@ val schemeEnvironment: Environment<Scheme>
       { literal -> compiled(nativeLine(literal.scheme), literal.schemeTypeLine) },
       { compiled -> compiled.resolveOrNull?.invoke(compiled) },
       { it.leoScriptLine },
-      { nativeTypesEnvironment })
+      { schemeTypesEnvironment })
 
 val Compiled<Scheme>.resolveOrNull: Compiled<Scheme>? get() =
   when (type) {
